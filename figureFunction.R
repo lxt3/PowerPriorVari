9ab46356062218a86fc82366e6b37655a8f2932b
Thompson <LXT@fda.gov>
2018-09-26 18:58:14 -0400

    function for setting up figure data (alpha0 and type I in this commit)

---
+++ figureFunction.R
@@ -0,0 +1,63 @@
+
+figureFunction<-function(n.use, path.stem) {
+
+fname.reg.KS1<-paste(path.stem,"resultsKSWeibull1siden",n.use,".RData", sep="") # KS 1-sided
+fname.reg.KS2<-paste(path.stem,"resultsKSWeibull2siden",n.use,".RData", sep="") # KS 2-sided
+fname.reg.Ord1<-paste(path.stem,"resultsSOWeibull1siden",n.use,".RData", sep="")  # Stoch ord 1-sided
+fname.reg.Ord2<-paste(path.stem,"resultsSOWeibull2siden",n.use,".RData", sep="")  # Stoch ord 2-sided
+
+
+# load and assign to objects
+
+# KS 1sided
+load(fname.reg.KS1)
+results.alpha.KS1side<-results.alpha.wb  # alpha
+results.prob.dropF.KS1side<-results.prob.dropF.wb  # type I error without dropping
+results.prob.dropT.KS1side<-results.prob.dropT.wb
+
+# make room for KS2 (for safety)
+rm(results.prob.dropF.wb, results.prob.dropT.wb,results.alpha.wb)
+
+
+# KS 2-sided
+load(fname.reg.KS2)
+results.alpha.KS2side<-results.alpha.wb
+results.prob.dropF.KS2side<-results.prob.dropF.wb  # type I error without dropping
+results.prob.dropT.KS2side<-results.prob.dropT.wb
+
+rm(results.prob.dropF.wb, results.prob.dropT.wb,results.alpha.wb)
+
+
+# SO 1-sided
+load(fname.reg.Ord1)
+results.alpha.SO1side<-results.alpha.wbord
+results.prob.dropF.SO1side<-results.prob.dropF.wbord  # type I error without dropping
+results.prob.dropT.SO1side<-results.prob.dropT.wbord
+
+# make room for SO2
+rm(results.alpha.wbord,results.prob.dropF.wbord,results.prob.dropT.wbord)
+
+
+# SO 2-sided
+load(fname.reg.Ord2)
+results.alpha.SO2side<-results.alpha.wbord
+results.prob.dropF.SO2side<-results.prob.dropF.wbord  # type I error without dropping
+results.prob.dropT.SO2side<-results.prob.dropT.wbord
+
+rm(results.alpha.wbord,results.prob.dropF.wbord,results.prob.dropT.wbord)
+
+
+# gather the vectors for output
+list(alpha=c(results.alpha.KS2side, 
+        results.alpha.KS1side,
+        results.alpha.SO1side,
+        results.alpha.SO2side), 
+     
+     typeI=c(results.prob.dropF.KS2side[1:20], results.prob.dropT.KS2side[1:20],
+     results.prob.dropF.KS1side[1:20], results.prob.dropT.KS1side[1:20],
+     results.prob.dropF.SO1side[1:20],results.prob.dropT.SO1side[1:20],
+     results.prob.dropF.SO2side[1:20], results.prob.dropT.SO2side[1:20])
+     
+     )
+
+}
