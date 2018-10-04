b38651071bf97c156301284357b4d194319a4867
Thompson <LXT@fda.gov>
2018-10-03 22:41:00 -0400

    added mpi version of running simulations

---
+++ mpi_version/modifiedMDICprogramMPI.R
@@ -0,0 +1,32 @@
+# One-arm trial (OPC) example
+
+# set different seeds per processor (diff=TRUE)
+comm.set.seed(seed., comm=0, diff=TRUE)  
+
+# Generate nsim current data sets and compute posterior inference using prior D0
+# Definition of FUN is in functionsSimulation.R
+ret<-task.pull(1:np,FUN, nsim=nsim, n=n, mu=mu, percent=percent,null=null,
+          prob.H1=prob.H1,nmcmc=nmcmc,max_alpha=max_alpha,D0=D0)
+
+
+if(comm.rank()==0){
+  ret.jobs<-lapply(ret,function(x){
+    c(res1=mean(x$res1), res2=mean(x$res2), alpha=mean(x$alpha), bias1=mean(x$bias1), bias2=mean(x$bias2))
+  })
+  ret.jobs<-Reduce("+",ret.jobs)/np # means
+  
+  # write results to sink
+  sink(sinkfname,split=T, append=T) 
+  cat("mu: ",mu, "\n")
+  cat("null: ",null, "\n")
+  cat("percent: ",percent, "\n")
+  cat("External?: ",external, "\n")
+  
+  print(ret.jobs)
+  sink()
+  
+  #ret.jobs<<-ret.jobs
+}
+
+
+
